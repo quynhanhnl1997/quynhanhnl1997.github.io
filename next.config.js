f4a8cd3a5951611a5e239f993a3daddf94a5117e
Anh Nguyen <nnthanhthi1997@gmail.com>
2022-12-11 18:55:45 +0700

    Update About Me page

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  trailingSlash: true,
-  assetPrefix: !debug ? 'https://quynhanhnl1997.github.io/' : ''
+  trailingSlash: true
 }
 
 module.exports = nextConfig
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,28 @@
+import Head from "next/head";
+import Link from "next/link";
+import AboutBody from "../components/aboutBody";
+import Container from "../components/container";
+import Layout from "../components/layout";
+
 export default function About() {
-  return <div>About</div>
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>About Me | Thi Nguyen</title>
+        </Head>
+
+        <Container>
+          <div>
+            <Link as={`/`} href="/">
+              <svg className="w-6 h-6 transition duration-300 bg-white hover:invert" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
+              </svg>
+            </Link>
+          </div>
+          <AboutBody />
+        </Container>
+      </Layout>
+    </>
+  )
 }
